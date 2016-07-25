5506665f85b3cb34fda6add849a3294c9fa771d3
unaibot <unaibot@gmail.com>
2016-07-25 02:10:17 +0200

    first commit

---
+++ Word1.rb
@@ -0,0 +1,7 @@
+
+
+array = "The method is called split".split()
+
+puts "#{array}"
+
+
---
+++ Word2.rb
@@ -0,0 +1,17 @@
+
+
+array = 'The method is called split'.split()
+
+puts "#{array}"
+
+marks = array.join(', ')
+
+puts "#{marks}"
+
+ marks.sort_by{ |x| x.upcase }
+
+puts "#{New_marks}"
+
+
+
+
